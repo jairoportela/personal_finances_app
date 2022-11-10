17eb792c401554813a4468936f2ddc5258426a57
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2022-11-09 22:23:28 -0500

    ADD: query data

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -8,13 +8,30 @@ class ApiProvider {
 
   final http.Client _httpClient;
 
-  Future<Map<String, dynamic>> readData(
-    String apiUrl,
+  Future<Map<String, dynamic>> readData({
+    required String apiUrl,
     Map<String, String>? headers,
-  ) async {
+  }) async {
     try {
       final response =
           await _httpClient.get(Uri.parse(apiUrl), headers: headers);
+      return json.decode(response.body) as Map<String, dynamic>;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>> queryData({
+    required String apiUrl,
+    Map<String, String>? headers,
+    Map<String, dynamic>? body,
+  }) async {
+    try {
+      final response = await _httpClient.post(
+        Uri.parse(apiUrl),
+        headers: headers,
+        body: json.encode(body),
+      );
       return json.decode(response.body) as Map<String, dynamic>;
     } catch (error) {
       rethrow;
