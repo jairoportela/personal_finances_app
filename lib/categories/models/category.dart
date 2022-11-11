1c544906f46880f387d0375c5777d5703862a866
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2022-11-11 14:17:31 -0500

    CHANGE: add to json

--- lib/categories/models/category.dart
+++ lib/categories/models/category.dart
@@ -29,29 +29,69 @@ class Categories {
   final String? nextCursor;
 }
 
-@JsonSerializable(createToJson: false)
+@JsonSerializable()
 class Category {
-  const Category({required this.id, required this.name, required this.icon});
+  const Category({
+    required this.id,
+    required this.name,
+    required this.icon,
+  });
 
   factory Category.fromJson(Map<String, dynamic> json) =>
       _$CategoryFromJson(json);
 
-  final String id;
-  @JsonKey(readValue: _readNameValue)
+  Map<String, dynamic> toJson() => _$CategoryToJson(this);
+  @JsonKey(includeIfNull: false)
+  final String? id;
+  @JsonKey(readValue: _readNameValue, toJson: _toJsonNameValue)
   final String name;
   final IconItem? icon;
 
   static String _readNameValue(Map<dynamic, dynamic> json, String _) {
     return json['properties']['Name']['title'][0]['text']['content'] as String;
   }
+
+  Category copyWith({
+    String? Function()? idFunction,
+    String? name,
+    IconItem? Function()? iconFunction,
+  }) =>
+      Category(
+        id: idFunction != null ? idFunction() : id,
+        name: name ?? this.name,
+        icon: iconFunction != null ? iconFunction() : icon,
+      );
+  static const empty = Category(
+    id: null,
+    name: '',
+    icon: null,
+  );
+
+  static Map<String, dynamic> _toJsonNameValue(String value) {
+    return {
+      'properties': {
+        'Name': {
+          'title': [
+            {
+              'text': {
+                'content': value,
+              }
+            }
+          ]
+        }
+      }
+    };
+  }
 }
 
-@JsonSerializable(createToJson: false)
+@JsonSerializable()
 class IconItem {
   const IconItem({required this.iconType, this.emoji, this.externalUrl});
 
   factory IconItem.fromJson(Map<String, dynamic> json) =>
       _$IconItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IconItemToJson(this);
 
   @JsonKey(unknownEnumValue: IconType.unknown, name: 'type')
   final IconType iconType;
@@ -76,12 +116,14 @@ enum IconType {
   final String type;
 }
 
-@JsonSerializable(createToJson: false)
+@JsonSerializable()
 class ExternalUrl {
   const ExternalUrl(this.url);
 
   factory ExternalUrl.fromJson(Map<String, dynamic> json) =>
       _$ExternalUrlFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ExternalUrlToJson(this);
 
   final String url;
 }
--- lib/categories/models/category.g.dart
+++ lib/categories/models/category.g.dart
@@ -7,24 +7,52 @@ part of 'category.dart';
 // **************************************************************************
 
 Category _$CategoryFromJson(Map<String, dynamic> json) => Category(
-      id: json['id'] as String,
-      name: Category._readNameValue(json, 'name'),
+      id: json['id'] as String?,
+      name: Category._readNameValue(json, 'name') as String,
       icon: json['icon'] == null
           ? null
           : IconItem.fromJson(json['icon'] as Map<String, dynamic>),
     );
 
+Map<String, dynamic> _$CategoryToJson(Category instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  val['name'] = Category._toJsonNameValue(instance.name);
+  val['icon'] = instance.icon;
+  return val;
+}
+
 IconItem _$IconItemFromJson(Map<String, dynamic> json) => IconItem(
-      iconType: $enumDecode(
-        _$IconTypeEnumMap,
-        json['type'],
-        unknownValue: IconType.unknown,
-      ),
+      iconType: $enumDecode(_$IconTypeEnumMap, json['type'],
+          unknownValue: IconType.unknown),
       emoji: json['emoji'] as String?,
       externalUrl: json['external'] == null
           ? null
           : ExternalUrl.fromJson(json['external'] as Map<String, dynamic>),
     );
+
+Map<String, dynamic> _$IconItemToJson(IconItem instance) {
+  final val = <String, dynamic>{
+    'type': _$IconTypeEnumMap[instance.iconType]!,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('emoji', instance.emoji);
+  writeNotNull('external', instance.externalUrl);
+  return val;
+}
 
 const _$IconTypeEnumMap = {
   IconType.emoji: 'emoji',
@@ -35,3 +63,8 @@ const _$IconTypeEnumMap = {
 ExternalUrl _$ExternalUrlFromJson(Map<String, dynamic> json) => ExternalUrl(
       json['url'] as String,
     );
+
+Map<String, dynamic> _$ExternalUrlToJson(ExternalUrl instance) =>
+    <String, dynamic>{
+      'url': instance.url,
+    };
