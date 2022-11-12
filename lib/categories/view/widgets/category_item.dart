0a4c54de3e7628814d0b77dbb57334199b212ce1
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2022-11-12 14:12:40 -0500

    CHANGE: adjust ontap

--- lib/categories/view/widgets/category_item.dart
+++ lib/categories/view/widgets/category_item.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_finances_app/categories/bloc/categories_overview_bloc.dart';
 import 'package:personal_finances_app/categories/models/category.dart';
-import 'package:personal_finances_app/categories/view/create_category_page.dart';
+import 'package:personal_finances_app/categories/view/category_page_create.dart';
 
-class CategoryItem extends StatelessWidget {
+class CategoryItem extends StatefulWidget {
   const CategoryItem({super.key, required this.item});
 
   final Category item;
 
   @override
+  State<CategoryItem> createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: item.icon != null
+      leading: widget.item.icon != null
           ? _LeadingIcon(
-              icon: item.icon!,
+              icon: widget.item.icon!,
             )
           : null,
-      title: Text(item.name),
-      onTap: () {
-        Navigator.of(context)
-            .pushNamed(CategoryCreatePage.routeName, arguments: item);
+      title: Text(widget.item.name),
+      onTap: () async {
+        final value = await Navigator.of(context)
+            .pushNamed(CategoryCreatePage.routeName, arguments: widget.item);
+        if (!mounted) return;
+        if (value != null && value == true) {
+          context
+              .read<CategoriesOverviewBloc>()
+              .add(CategoriesOverviewDataRequested());
+        }
       },
     );
   }
